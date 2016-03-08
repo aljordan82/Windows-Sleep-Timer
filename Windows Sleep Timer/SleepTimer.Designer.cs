201fb48201efb8ad05c40aca2b1bd41e18217987
aljordan82 <aljordan82@gmail.com>
2016-03-07 20:09:42 -0500

    Sleep Timer
    First upload

---
+++ Windows Sleep Timer/SleepTimer.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Windows_Sleep_Timer
+{
+    partial class SleepTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hoursLbl = new System.Windows.Forms.Label();
+            this.hoursInput = new System.Windows.Forms.NumericUpDown();
+            this.minutesLbl = new System.Windows.Forms.Label();
+            this.minutesField = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.secondsField = new System.Windows.Forms.NumericUpDown();
+            this.sleepBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.hoursInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minutesField)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondsField)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hoursLbl
+            // 
+            this.hoursLbl.AutoSize = true;
+            this.hoursLbl.Location = new System.Drawing.Point(12, 9);
+            this.hoursLbl.Name = "hoursLbl";
+            this.hoursLbl.Size = new System.Drawing.Size(35, 13);
+            this.hoursLbl.TabIndex = 0;
+            this.hoursLbl.Text = "Hours";
+            // 
+            // hoursInput
+            // 
+            this.hoursInput.Location = new System.Drawing.Point(15, 24);
+            this.hoursInput.Name = "hoursInput";
+            this.hoursInput.Size = new System.Drawing.Size(35, 20);
+            this.hoursInput.TabIndex = 1;
+            this.hoursInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // minutesLbl
+            // 
+            this.minutesLbl.AutoSize = true;
+            this.minutesLbl.Location = new System.Drawing.Point(65, 9);
+            this.minutesLbl.Name = "minutesLbl";
+            this.minutesLbl.Size = new System.Drawing.Size(44, 13);
+            this.minutesLbl.TabIndex = 2;
+            this.minutesLbl.Text = "Minutes";
+            // 
+            // minutesField
+            // 
+            this.minutesField.Location = new System.Drawing.Point(68, 24);
+            this.minutesField.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.minutesField.Name = "minutesField";
+            this.minutesField.Size = new System.Drawing.Size(35, 20);
+            this.minutesField.TabIndex = 3;
+            this.minutesField.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Seconds";
+            // 
+            // secondsField
+            // 
+            this.secondsField.Location = new System.Drawing.Point(122, 24);
+            this.secondsField.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.secondsField.Name = "secondsField";
+            this.secondsField.Size = new System.Drawing.Size(35, 20);
+            this.secondsField.TabIndex = 5;
+            this.secondsField.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // sleepBtn
+            // 
+            this.sleepBtn.Location = new System.Drawing.Point(180, 22);
+            this.sleepBtn.Name = "sleepBtn";
+            this.sleepBtn.Size = new System.Drawing.Size(60, 23);
+            this.sleepBtn.TabIndex = 6;
+            this.sleepBtn.Text = "Sleep";
+            this.sleepBtn.UseVisualStyleBackColor = true;
+            this.sleepBtn.Click += new System.EventHandler(this.sleepBtn_Click);
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Location = new System.Drawing.Point(180, 22);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(60, 23);
+            this.cancelBtn.TabIndex = 7;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // SleepTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 51);
+            this.Controls.Add(this.secondsField);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.minutesField);
+            this.Controls.Add(this.minutesLbl);
+            this.Controls.Add(this.hoursInput);
+            this.Controls.Add(this.hoursLbl);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.sleepBtn);
+            this.MaximumSize = new System.Drawing.Size(280, 90);
+            this.MinimumSize = new System.Drawing.Size(280, 90);
+            this.Name = "SleepTimer";
+            this.Text = "Sleep Timer";
+            ((System.ComponentModel.ISupportInitialize)(this.hoursInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minutesField)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondsField)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label hoursLbl;
+        private System.Windows.Forms.NumericUpDown hoursInput;
+        private System.Windows.Forms.Label minutesLbl;
+        private System.Windows.Forms.NumericUpDown minutesField;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown secondsField;
+        private System.Windows.Forms.Button sleepBtn;
+        private System.Windows.Forms.Button cancelBtn;
+    }
+}
+
---
+++ Windows Sleep Timer/SleepTimer.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Sleep_Timer
+{
+    public partial class SleepTimer : Form
+    {
+        private int counter = 0;
+        private Timer timer;
+        public SleepTimer()
+        {
+            InitializeComponent();
+            this.cancelBtn.Visible = false;
+        }
+
+        private void sleepBtn_Click(object sender, EventArgs e)
+        {
+            int time = GetTime();
+            if (time <= 0)
+            {
+                return;
+            }
+            SleepFn(time);
+            
+            this.ToggleInputFileds(false);
+            this.sleepBtn.Visible = false;
+            this.cancelBtn.Visible = true;
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            CancelTimer();
+            this.ToggleInputFileds(true);
+            this.sleepBtn.Visible = true;
+            this.cancelBtn.Visible = false;
+        }
+
+        private void ToggleInputFileds( bool state )
+        {
+            this.hoursInput.Enabled = state;
+            this.minutesField.Enabled = state;
+            this.secondsField.Enabled = state;
+        }  
+
+        private int GetTime()
+        {
+            int seconds = (int) this.secondsField.Value;
+            int minutesToSeconds = (int)this.minutesField.Value * 60;
+            int hoursToSeconds = (int)this.hoursInput.Value * 3600;
+
+            var time = seconds + minutesToSeconds + hoursToSeconds;
+            return time;
+        }
+        
+        private void SleepFn(int time)
+        {
+           
+            if (time <=0)
+            {
+                return ;
+            }
+            counter = time;
+
+            timer = new Timer();
+            timer.Tick += new EventHandler(timer1_Tick);
+            timer.Interval = 1000; // 1 second
+            timer.Start();
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter--;
+
+            if (counter == 0)
+            {
+                CancelTimer();
+                Application.SetSuspendState(PowerState.Suspend, true, true);
+                //Environment.Exit(0);
+            }
+
+            TimeSpan ts = TimeSpan.FromSeconds(counter);
+
+            this.secondsField.Value = ts.Seconds;
+            this.minutesField.Value = ts.Minutes;
+            this.hoursInput.Value = ts.Hours;
+        }
+
+        private void CancelTimer()
+        {
+            timer.Stop();
+            timer.Dispose();
+            timer = null;
+            GC.Collect();
+        }
+
+        private void SleepTimerFrm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            CancelTimer();
+            System.Diagnostics.Process.Start("shutdown", "/a");
+        }
+    }
+}
